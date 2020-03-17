672d794eceb729d01c3dca6d4c114a37c93b9c26
Falways <1914783590@qq.com>
2020-03-17 12:41:05 +0800

    u

---
+++ c_grammar_sugar/cmake-build-debug/CMakeFiles/c_grammar_sugar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_grammar_sugar.dir/main.c.o"
+  "c_grammar_sugar"
+  "c_grammar_sugar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_grammar_sugar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c_superset_grammar_sugar/cmake-build-debug/CMakeFiles/grammar_sugar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grammar_sugar.dir/main.cpp.o"
+  "grammar_sugar"
+  "grammar_sugar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grammar_sugar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
